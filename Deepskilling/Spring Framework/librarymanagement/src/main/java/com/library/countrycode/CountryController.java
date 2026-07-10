16571988034d52574759ef7f8c4c37c8d4d6ae7f
R Atchaya <rj.atchi@gmail.com>
2026-07-10 13:16:57 +0530

    Completed REST API to fetch country by country code

--- Deepskilling/Spring Framework/librarymanagement/src/main/java/com/library/countrycode/CountryController.java
+++ Deepskilling/Spring Framework/librarymanagement/src/main/java/com/library/countrycode/CountryController.java
@@ -1,5 +1,16 @@
 package com.library.countrycode;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class CountryController {
 
+    @Autowired
+    private CountryService service;
+
+    @GetMapping("/country/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return service.getCountry(code);
+    }
 }
--- Deepskilling/Spring Framework/librarymanagement/src/main/java/com/library/countrycode/CountryService.java
+++ Deepskilling/Spring Framework/librarymanagement/src/main/java/com/library/countrycode/CountryService.java
@@ -1,5 +1,22 @@
 package com.library.countrycode;
 
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class CountryService {
 
+    private final Map<String, Country> countries = new HashMap<>();
+
+    public CountryService() {
+        countries.put("IN", new Country("IN", "India"));
+        countries.put("US", new Country("US", "United States"));
+        countries.put("JP", new Country("JP", "Japan"));
+    }
+
+    public Country getCountry(String code) {
+        return countries.get(code.toUpperCase());
+    }
 }
