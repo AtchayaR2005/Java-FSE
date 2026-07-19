50d8171b59ff8c2504b50edd6fd248ca1ad86133
R Atchaya <rj.atchi@gmail.com>
2026-07-19 15:51:19 +0530

    Completed React HOL6 - Trainers App using React Router

---
+++ Deepskilling/React/ReactHOL6/trainersapp/src/Home.js
@@ -0,0 +1,17 @@
+function Home(){
+
+return(
+
+<div>
+
+<h1>Welcome to Trainers App</h1>
+
+<p>Select Trainers menu to view trainers.</p>
+
+</div>
+
+);
+
+}
+
+export default Home;
---
+++ Deepskilling/React/ReactHOL6/trainersapp/src/TrainersMock.js
@@ -0,0 +1,32 @@
+const trainers = [
+
+{
+id:1,
+name:"John",
+email:"john@gmail.com",
+phone:"9876543210",
+technology:"React",
+skills:"React, JavaScript"
+},
+
+{
+id:2,
+name:"David",
+email:"david@gmail.com",
+phone:"9876543211",
+technology:"Java",
+skills:"Spring Boot"
+},
+
+{
+id:3,
+name:"Alice",
+email:"alice@gmail.com",
+phone:"9876543212",
+technology:"Python",
+skills:"Django"
+}
+
+];
+
+export default trainers;
