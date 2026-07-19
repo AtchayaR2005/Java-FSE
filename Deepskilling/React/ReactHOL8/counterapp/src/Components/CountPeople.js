087d7c27863e28babb9f9bdf3c3ab22b0de2ffc2
R Atchaya <rj.atchi@gmail.com>
2026-07-19 16:02:27 +0530

    Completed React HOL8 - Counter App using React State

---
+++ Deepskilling/React/ReactHOL8/counterapp/src/Components/CountPeople.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class CountPeople extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+
+            entryCount: 0,
+
+            exitCount: 0
+
+        };
+
+    }
+
+    UpdateEntry = () => {
+
+        this.setState({
+
+            entryCount: this.state.entryCount + 1
+
+        });
+
+    };
+
+    UpdateExit = () => {
+
+        this.setState({
+
+            exitCount: this.state.exitCount + 1
+
+        });
+
+    };
+
+    render() {
+
+        return (
+
+            <div style={{ textAlign: "center" }}>
+
+                <h1>Mall Entry Counter</h1>
+
+                <h2>Entry Count : {this.state.entryCount}</h2>
+
+                <button onClick={this.UpdateEntry}>
+                    Login
+                </button>
+
+                <br /><br />
+
+                <h2>Exit Count : {this.state.exitCount}</h2>
+
+                <button onClick={this.UpdateExit}>
+                    Exit
+                </button>
+
+            </div>
+
+        );
+
+    }
+
+}
+
+export default CountPeople;
