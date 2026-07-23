b146c8b66259a8015c81c96de9ec07c7f870e236
Your Name <rj.atchi@gmail.com>
2026-07-23 21:07:35 +0530

    feat(HOL3): API Gateway - Spring Cloud Gateway routing to all microservices (port 8080)

---
+++ Deepskilling/Spring Framework/Microservices/HOL3-ApiGateway/api-gateway/src/main/java/com/hol3/apigateway/config/GatewayConfig.java
@@ -0,0 +1,11 @@
+﻿package com.hol3.apigateway.config;
+
+import org.springframework.cloud.gateway.server.mvc.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    // Spring Cloud Gateway routes are configured in application.yml
+    // Additional custom filters can be added here
+}
